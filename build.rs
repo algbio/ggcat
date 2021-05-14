5717666a88636418cfd67eefa2c56ec7c7536444
Andrea Cracco <guilucand@gmail.com>
2021-05-14 12:47:15 +0200

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() {
     println!("cargo:rustc-link-search=native={}", "./libdeflate");
     println!("cargo:rustc-link-lib=static=deflate");
+    println!("cargo:rustc-link-search=native={}", ".");
+    println!("cargo:rerun-if-changed=src/");
 }
