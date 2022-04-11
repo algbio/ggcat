270e7688d53d6d80c0cf6e44acb0edd92073e238
Andrea Cracco <guilucand@gmail.com>
2022-04-11 15:18:11 +0300

    Test read + hmap

--- src/assemble_pipeline/parallel_kmers_merge.rs
+++ src/assemble_pipeline/parallel_kmers_merge.rs
@@ -305,7 +305,6 @@ impl<'x, H: HashFunctionFactory, MH: HashFunctionFactory, CX: ColorsManager>
             _,
             <ParallelKmersMergeFactory<H, MH, CX> as KmersTransformExecutorFactory>::FLAGS_COUNT,
         >, _>(Vec::new(), |(flags, color, read)| {
-            return;
             let hashes = MH::new(read, k);
 
             let last_hash_pos = read.bases_count() - k;
