512323656a7c158636c437ea5e7a3c9373bb375b
Andrea Cracco <guilucand@gmail.com>
2022-05-09 16:28:29 +0300

    Add maximum memory usage tracking

--- parallel-processor-rs/src/execution_manager/memory_tracker.rs
+++ parallel-processor-rs/src/execution_manager/memory_tracker.rs
@@ -2,13 +2,14 @@ use crate::execution_manager::executor::Executor;
 use crate::execution_manager::packet::PacketTrait;
 use crate::memory_data_size::MemoryDataSize;
 use dashmap::DashMap;
+use std::cmp::max;
 use std::io::stdout;
 use std::marker::PhantomData;
 use std::sync::Arc;
 
 pub struct MemoryTrackerManager {
-    packet_sizes: DashMap<*const str, (usize, usize)>,
-    executors_sizes: DashMap<*const str, (usize, usize)>,
+    packet_sizes: DashMap<*const str, ((usize, usize), usize)>,
+    executors_sizes: DashMap<*const str, ((usize, usize), usize)>,
 }
 
 unsafe impl Sync for MemoryTrackerManager {}
@@ -31,9 +32,13 @@ impl MemoryTrackerManager {
             let mut entry = self
                 .packet_sizes
                 .entry(std::any::type_name::<T>() as *const str)
-                .or_insert((0, 0));
-            entry.value_mut().0 += packet.get_size();
-            entry.value_mut().1 += 1;
+                .or_insert(((0, 0), 0));
+            entry.value_mut().0 .0 += packet.get_size();
+            entry.value_mut().0 .1 += 1;
+
+            let crt_val = entry.value_mut().0 .0;
+            let max_val = entry.value().1;
+            entry.value_mut().1 = max(max_val, crt_val);
         }
     }
     pub fn remove_queue_packet<T: PacketTrait>(&self, packet: &T) {
@@ -42,8 +47,8 @@ impl MemoryTrackerManager {
                 .packet_sizes
                 .get_mut(&(std::any::type_name::<T>() as *const str))
                 .unwrap();
-            entry.value_mut().0 -= packet.get_size();
-            entry.value_mut().1 -= 1;
+            entry.value_mut().0 .0 -= packet.get_size();
+            entry.value_mut().0 .1 -= 1;
         }
     }
 
@@ -84,20 +89,22 @@ impl MemoryTrackerManager {
         println!("Executors usages:");
         for executor in self.executors_sizes.iter() {
             println!(
-                "\t{} ==> {} with {} instances",
+                "\t{} ==> {:.2} with {} instances [MAX {:.2}]",
                 Self::get_pretty_name(*executor.key()),
-                MemoryDataSize::from_bytes(executor.value().0),
-                executor.value().1
+                MemoryDataSize::from_bytes(executor.value().0 .0),
+                executor.value().0 .1,
+                MemoryDataSize::from_bytes(executor.value().1)
             );
         }
 
         println!("Packets in queue:");
         for packet in self.packet_sizes.iter() {
             println!(
-                "\t{} ==> {} with {} instances",
+                "\t{} ==> {:.2} with {} instances [MAX {:.2}]",
                 Self::get_pretty_name(*packet.key()),
-                MemoryDataSize::from_bytes(packet.value().0),
-                packet.value().1
+                MemoryDataSize::from_bytes(packet.value().0 .0),
+                packet.value().0 .1,
+                MemoryDataSize::from_bytes(packet.value().1)
             );
         }
     }
@@ -130,9 +137,10 @@ impl<E: Executor> MemoryTracker<E> {
         manager
             .executors_sizes
             .entry(std::any::type_name::<E>() as *const str)
-            .or_insert((0, 0))
+            .or_insert(((0, 0), 0))
             .value_mut()
-            .1 += 1;
+            .0
+             .1 += 1;
         MemoryTracker {
             manager,
             last_memory_usage: 0,
@@ -147,9 +155,13 @@ impl<E: Executor> MemoryTracker<E> {
             .executors_sizes
             .get_mut(&(std::any::type_name::<E>() as *const str))
             .unwrap();
-        entry.value_mut().0 -= self.last_memory_usage;
-        entry.value_mut().0 += new_memory_usage;
+        entry.value_mut().0 .0 -= self.last_memory_usage;
+        entry.value_mut().0 .0 += new_memory_usage;
         self.last_memory_usage = new_memory_usage;
+
+        let crt_val = entry.value_mut().0 .0;
+        let max_val = entry.value().1;
+        entry.value_mut().1 = max(max_val, crt_val);
     }
 }
 
@@ -160,7 +172,7 @@ impl<E: Executor> Drop for MemoryTracker<E> {
             .executors_sizes
             .get_mut(&(std::any::type_name::<E>() as *const str))
             .unwrap();
-        entry.value_mut().0 -= self.last_memory_usage;
-        entry.value_mut().1 -= 1;
+        entry.value_mut().0 .0 -= self.last_memory_usage;
+        entry.value_mut().0 .1 -= 1;
     }
 }
