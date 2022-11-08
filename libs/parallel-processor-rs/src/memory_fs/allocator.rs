579b5ceb8f63eba535f504263fe1c8e2537e5bae
Andrea Cracco <guilucand@gmail.com>
2022-11-08 01:55:53 +0100

    Add exponential oom allocation

--- libs/parallel-processor-rs/src/memory_fs/allocator.rs
+++ libs/parallel-processor-rs/src/memory_fs/allocator.rs
@@ -398,13 +398,18 @@ impl ChunksAllocator {
 
                         // We're out of memory, let's try to allocate another chunk
                         if let Some(mut free_chunk_pool) = self.chunks.try_lock() {
+                            let alloc_multiplier =
+                                1 << (self.buffers_list.lock().len().saturating_sub(1));
+
                             let extra_chunks_count = OUT_OF_MEMORY_ALLOCATION_SIZE.as_bytes()
+                                * alloc_multiplier
                                 / self.chunk_usable_size.load(Ordering::Relaxed);
                             let chunks_iter = self.allocate_contiguous_chunk(extra_chunks_count);
                             free_chunk_pool.extend(chunks_iter);
                             println!(
                                 "Allocated {} extra chunks for temporary files ({})",
-                                extra_chunks_count, OUT_OF_MEMORY_ALLOCATION_SIZE
+                                extra_chunks_count,
+                                OUT_OF_MEMORY_ALLOCATION_SIZE * alloc_multiplier as f64
                             );
                         }
                         // Reset the tries counter
