ba77031f6f6c8b2c9b890dfffc58f7e8b77e1b72
Andrea Cracco <guilucand@gmail.com>
2022-11-15 13:53:37 +0100

    Add working graph building from in-memory sequences

--- crates/capi/src/lib.rs
+++ crates/capi/src/lib.rs
@@ -1,5 +1,10 @@
+use std::slice::from_raw_parts;
+use std::sync::Arc;
 use std::{mem::transmute, path::PathBuf};
 
+use ggcat_api::{
+    ColorIndexType, DnaSequence, DnaSequencesFileType, DynamicSequencesStream, SequenceInfo,
+};
 use ggcat_api::{ExtraElaboration, GGCATConfig, GGCATInstance, GeneralSequenceBlockData};
 
 #[repr(transparent)]
@@ -29,10 +34,10 @@ fn ggcat_create(config: ffi::GGCATConfigFFI) -> &'static GGCATInstanceFFI {
     unsafe { std::mem::transmute(instance) }
 }
 
-fn ggcat_build_from_files(
+fn ggcat_build(
     instance: &'static GGCATInstanceFFI,
-    // The input files
-    input_files: &[String],
+    // The input blocks
+    input_blocks: Vec<GeneralSequenceBlockData>,
 
     // The output file
     output_file: String,
@@ -67,17 +72,7 @@ fn ggcat_build_from_files(
     instance
         .0
         .build_graph(
-            input_files
-                .iter()
-                .map(|f| {
-                    if f.ends_with(".gfa") {
-                        todo!("GFA support is not implemented yet");
-                        // GeneralSequenceBlockData::GFA() // PathBuf::from(f))
-                    } else {
-                        GeneralSequenceBlockData::FASTA(PathBuf::from(f))
-                    }
-                })
-                .collect(),
+            input_blocks,
             PathBuf::from(output_file),
             if color_names.len() > 0 {
                 Some(color_names)
@@ -106,6 +101,185 @@ fn ggcat_build_from_files(
         .to_str()
         .unwrap()
         .to_string()
+}
+
+fn ggcat_build_from_files(
+    instance: &'static GGCATInstanceFFI,
+    // The input files
+    input_files: &[String],
+
+    // The output file
+    output_file: String,
+
+    // The names of the colors, ordered by color index
+    color_names: &[String],
+
+    // Specifies the k-mers length
+    kmer_length: usize,
+    // The threads to be used
+    threads_count: usize,
+    // Treats reverse complementary kmers as different
+    forward_only: bool,
+    // Overrides the default m-mers (minimizers) length
+    minimizer_length: usize,
+
+    // Enable colors
+    colors: bool,
+
+    // Minimum multiplicity required to keep a kmer
+    min_multiplicity: usize,
+
+    // Extra elaboration step
+    extra_elab: usize,
+) -> String {
+    ggcat_build(
+        instance,
+        input_files
+            .iter()
+            .map(|f| {
+                if f.ends_with(".gfa") {
+                    todo!("GFA support is not implemented yet");
+                    // GeneralSequenceBlockData::GFA() // PathBuf::from(f))
+                } else {
+                    GeneralSequenceBlockData::FASTA(PathBuf::from(f))
+                }
+            })
+            .collect(),
+        output_file,
+        color_names,
+        kmer_length,
+        threads_count,
+        forward_only,
+        minimizer_length,
+        colors,
+        min_multiplicity,
+        extra_elab,
+    )
+}
+
+fn ggcat_build_from_streams(
+    instance: &'static GGCATInstanceFFI,
+    // The input streams
+    input_streams: &[ffi::InputStreamFFI],
+
+    // The output file
+    output_file: String,
+
+    // The names of the colors, ordered by color index
+    color_names: &[String],
+
+    // Specifies the k-mers length
+    kmer_length: usize,
+    // The threads to be used
+    threads_count: usize,
+    // Treats reverse complementary kmers as different
+    forward_only: bool,
+    // Overrides the default m-mers (minimizers) length
+    minimizer_length: usize,
+
+    // Enable colors
+    colors: bool,
+
+    // Minimum multiplicity required to keep a kmer
+    min_multiplicity: usize,
+
+    // Extra elaboration step
+    extra_elab: usize,
+) -> String {
+    struct SequencesStreamFFI {
+        // extern "C" void (*read_block)(uintptr_t block, bool copy_ident_data, size_t partial_read_copyback, uintptr_t callback, uintptr_t callback_context);
+        //      with void callback(uintptr_t callback_context, DnaSequenceFFI sequence, SequenceInfoFFI info);
+        virtual_read_block: extern "C" fn(
+            block: usize,
+            copy_ident_data: bool,
+            partial_read_copyback: usize,
+            callback: extern "C" fn(
+                callback_context: usize,
+                sequence: DnaSequenceFFI,
+                info: SequenceInfoFFI,
+            ),
+            callback_context: usize,
+        ),
+        // extern "C" uint64_t (*estimated_base_count)(uintptr_t block);
+        virtual_estimated_base_count: extern "C" fn(block: usize) -> u64,
+    }
+
+    impl DynamicSequencesStream for SequencesStreamFFI {
+        fn read_block(
+            &self,
+            block: usize,
+            copy_ident_data: bool,
+            partial_read_copyback: Option<usize>,
+            mut callback: &mut dyn FnMut(DnaSequence, SequenceInfo),
+        ) {
+            extern "C" fn callback_wrapper(
+                callback_ptr: usize,
+                sequence: DnaSequenceFFI,
+                info: SequenceInfoFFI,
+            ) {
+                let callback = unsafe {
+                    &mut *(callback_ptr as *mut &mut dyn FnMut(DnaSequence, SequenceInfo))
+                };
+                callback(
+                    DnaSequence {
+                        ident_data: unsafe {
+                            from_raw_parts(sequence.ident_data, sequence.ident_data_len)
+                        },
+                        seq: unsafe { from_raw_parts(sequence.seq, sequence.seq_len) },
+                        format: match sequence.format {
+                            DnaSequencesFileTypeFFI::FASTA => DnaSequencesFileType::FASTA,
+                            DnaSequencesFileTypeFFI::FASTQ => DnaSequencesFileType::FASTQ,
+                            DnaSequencesFileTypeFFI::GFA => DnaSequencesFileType::GFA,
+                            DnaSequencesFileTypeFFI::BINARY => DnaSequencesFileType::BINARY,
+                        },
+                    },
+                    SequenceInfo {
+                        color: Some(info.color),
+                    },
+                );
+            }
+
+            let callback_ptr = (&mut callback) as *mut _ as usize;
+            (self.virtual_read_block)(
+                block,
+                copy_ident_data,
+                partial_read_copyback.unwrap_or(0),
+                callback_wrapper,
+                callback_ptr,
+            );
+        }
+
+        fn estimated_base_count(&self, block: usize) -> u64 {
+            (self.virtual_estimated_base_count)(block)
+        }
+    }
+
+    ggcat_build(
+        instance,
+        input_streams
+            .iter()
+            .map(|s| {
+                GeneralSequenceBlockData::Dynamic((
+                    Arc::new(unsafe {
+                        SequencesStreamFFI {
+                            virtual_read_block: transmute(s.virtual_read_block),
+                            virtual_estimated_base_count: transmute(s.virtual_estimated_base_count),
+                        }
+                    }),
+                    s.block_data,
+                ))
+            })
+            .collect(),
+        output_file,
+        color_names,
+        kmer_length,
+        threads_count,
+        forward_only,
+        minimizer_length,
+        colors,
+        min_multiplicity,
+        extra_elab,
+    )
 }
 
 /// Queries a (optionally) colored graph with a specific set of sequences as queries
@@ -231,6 +405,31 @@ fn ggcat_dump_unitigs(
     )
 }
 
+static_assertions::assert_eq_size!(ColorIndexType, u32);
+
+#[derive(Debug)]
+#[repr(C)]
+pub enum DnaSequencesFileTypeFFI {
+    FASTA = 0,
+    FASTQ = 1,
+    GFA = 2,
+    BINARY = 3,
+}
+
+#[repr(C)]
+pub struct DnaSequenceFFI {
+    pub ident_data: *const u8,
+    pub ident_data_len: usize,
+    pub seq: *const u8,
+    pub seq_len: usize,
+    pub format: DnaSequencesFileTypeFFI,
+}
+
+#[repr(C)]
+pub struct SequenceInfoFFI {
+    pub color: u32, // ColorIndexType
+}
+
 #[cxx::bridge]
 mod ffi {
     /// Main config of GGCAT. This config is global and should be passed to GGCATInstance::create
@@ -262,6 +461,15 @@ mod ffi {
         pub stats_file: String,
     }
 
+    pub struct InputStreamFFI {
+        // extern "C" void (*read_block)(uintptr_t block, bool copy_ident_data, size_t partial_read_copyback, uintptr_t callback, uintptr_t callback_context);
+        //      with void callback(uintptr_t callback_context, DnaSequenceFFI sequence, SequenceInfoFFI info);
+        pub virtual_read_block: usize,
+        // extern "C" size_t (*estimated_base_count)(uintptr_t block);
+        pub virtual_estimated_base_count: usize,
+        pub block_data: usize,
+    }
+
     extern "Rust" {
         type GGCATInstanceFFI;
 
@@ -273,6 +481,37 @@ mod ffi {
             instance: &'static GGCATInstanceFFI,
             // The input files
             input_files: &[String],
+
+            // The output file
+            output_file: String,
+
+            // The names of the colors, ordered by color index
+            color_names: &[String],
+
+            // Specifies the k-mers length
+            kmer_length: usize,
+            // The threads to be used
+            threads_count: usize,
+            // Treats reverse complementary kmers as different
+            forward_only: bool,
+            // Overrides the default m-mers (minimizers) length
+            minimizer_length: usize,
+
+            // Enable colors
+            colors: bool,
+
+            // Minimum multiplicity required to keep a kmer
+            min_multiplicity: usize,
+
+            // Extra elaboration step
+            extra_elab: usize,
+        ) -> String;
+
+        /// Builds a new graph from the given input streams, with the specified parameters
+        fn ggcat_build_from_streams(
+            instance: &'static GGCATInstanceFFI,
+            // The input streams
+            input_streams: &[InputStreamFFI],
 
             // The output file
             output_file: String,
