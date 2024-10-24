dc91c778de7d82cba54874b49b8f4894ac5e8eaf
Andrea Cracco <guilucand@gmail.com>
2024-10-24 16:49:29 +0300

    Remove old conda recipes

--- conda/build.sh
+++
@@ -1,6 +0,0 @@
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain nightly -y
-git clone https://github.com/algbio/ggcat.git --recursive
-pushd ggcat
-    git checkout "v1.0_beta.1"
-    cargo install --path crates/cmdline/ --locked --root "${PREFIX}"
-popd
